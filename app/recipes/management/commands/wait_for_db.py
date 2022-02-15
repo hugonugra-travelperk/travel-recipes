78a55b337a130619cf942fe18fdbc7b4905cd348
hugo.nugra <hugo.nugra@travelperk.com>
2022-02-15 18:11:35 +0100

    Add database configuration

---
+++ app/recipes/management/commands/wait_for_db.py
@@ -0,0 +1,20 @@
+import time
+from django.db import connections
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """The Django command to pause execution unit database is available"""
+
+    def handle(self, *args, **options):
+        self.stdout.write('Waiting for the database')
+        db_conn = None
+        while not db_conn:
+            try:
+                db_conn = connections['default']
+            except OperationalError:
+                self.stdout.write('The Database is not available, waiting 1 second')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Waiting for the database'))
